c886cd87a192b7a85ec76bf619b819c6f12b71a2
Muhammed Sihebi <muhammednurpro@gmail.com>
2022-04-11 02:34:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(argu = 'roller-skate'){
+    return "This Saturday, I want to " + argu + "!"; 
+}
+
+function mondayWork(argu = 'go to the office'){
+    return `This Monday, I will ${argu}.`
+}
+
+function wrapAdjective(v = "*"){
+    return function(argu = "special"){
+        return  `You are ${v}${argu}${v}!`
+    }
+}
